a7a27747db9ca2b1d21a7a7a15c1370d4a61d48f
Ivens Lima <ivens_cml@outlook.com>
2020-12-09 22:38:06 -0300

    Added chapter 6 code in Cap5 folder (because the book said so)

--- Cap5/PacktLibrary/Person.cs
+++ Cap5/PacktLibrary/Person.cs
@@ -4,7 +4,7 @@ using static System.Console;
 
 namespace Packt.Shared
 {
-    public partial class Person : object
+    public partial class Person : IComparable<Person>
     {
       // fields
       public string Name;
@@ -86,5 +86,80 @@ namespace Packt.Shared
         y++;
         z++;
       }
+
+      //---------------------------------------------------------------
+      //  From here on, it's from chapter 6
+      //---------------------------------------------------------------
+
+
+      //static method to multiply
+      public static Person Procreate(Person p1, Person p2)
+      {
+        var baby = new Person{
+          Name = $"Baby of {p1.Name} and {p2.Name}"
+        };
+
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+
+        return baby;
+      }
+
+      // intance method to multiply
+      public Person ProcreateWith(Person partner)
+      {
+        return Procreate(this, partner);
+      }
+
+      // operator to multiply
+      public static Person operator *(Person p1, Person p2)
+      {
+        return Person.Procreate(p1, p2);
+      }
+
+      // method with a local function
+      public static int Factorial(int number)
+      {
+        if(number < 0)
+        {
+          throw new ArgumentException($"{nameof(number)} cannot be less than zero.");
+        }
+        return localFactorial(number);
+
+        int localFactorial(int localNumber)  // local function
+        {
+          if (localNumber < 1) return 1;
+          return localNumber * localFactorial(localNumber - 1);
+        }
+      }
+
+      // event delegate field
+      public event EventHandler Shout;
+
+      // data field
+      public int AngerLevel;
+
+      // method
+      public void Poke()
+      {
+        AngerLevel++;
+        if (AngerLevel >= 3)
+        {
+          // if someone is listening...
+          // if (Shout != null)
+          // {
+          //   // ...the call the delegate
+          //   Shout(this, EventArgs.Empty);
+          // }
+
+          // alternatively
+          Shout?.Invoke(this, EventArgs.Empty);
+        }
+      }
+
+    public int CompareTo(Person other)
+    {
+      return Name.CompareTo(other.Name);
     }
+  }
 }
